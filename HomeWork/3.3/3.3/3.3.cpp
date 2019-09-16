8f0c763177ea736b3c1242e19fbeee4772f603e4
Sasha Honin <777@6020322.ru>
2019-09-16 13:30:30 +0300

    3

---
+++ HomeWork/3.3/3.3/3.3.cpp
@@ -0,0 +1,25 @@
+﻿// 3.3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	string path = "text3.3.txt";
+	int a;
+	ifstream F;
+	F.open(path);
+	if(!F.is_open())
+	{
+		cout << "Ошибка открытия файла " << endl;
+	}
+	else
+	{
+		cout << "Файл открыт" << endl;
+		F >> a;
+		cout << a;
+	}
+}
