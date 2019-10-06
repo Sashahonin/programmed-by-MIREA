bb5ae13d9ea8fc6d1041e2f5b9cb8fb69c099b80
Sashahonin <777@6020322.ru>
2019-10-06 17:31:33 +0300

    list/2

--- HomeWork/Listok/2/2/2.cpp
+++ HomeWork/Listok/2/2/2.cpp
@@ -1,56 +1,58 @@
-﻿
+﻿// 2,2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
 #include <iostream>
 #include <ctime>
 using namespace std;
 
 int main()
 {
+	int i;
 	setlocale(LC_ALL, "Russian");
 	srand(time(NULL));
 	int mas[100];
-	for (int i = 0; i < 100; i++)
+	for (i = 0; i < 100; i++)
 	{
 		mas[i] = rand() % 999 + 100;
 	}
 	cout << "Массив первый" << endl;
-	for (int i = 0; i < 100; i++)
+	for (i = 0; i < 100; i++)
 	{
 		cout << mas[i] << " ";
 		if (i % 15 == 0 && i > 0)
 		{
 			cout << endl;
 		}
-		
-
 	}
-	
 	cout << "Конец первого массива" << endl;
 	int k = 0;
 	int z = 0;
 	int mas2[100];
-	int mas3[100];   
-
-	for (int i = 0; i < 100; i++)
+	int mas3[100];
+	int num, num2, num3;
+	for (i = 0; i < 100; i++)
 	{
-		
-		if (mas[i]% 10 % 2 == 0 $$ mas[i] % 10 % 2 == 0 && mas[i] % 10 % 2 == 0)
+		num = mas[i] % 10;
+		num2 = mas[i] % 100 / 10;
+		num3 = mas[i] / 100;
+		if ((num % 2 == 0) && (num2 % 2 == 0) && (num3 % 2 == 0))
 		{
 			mas2[k] = mas[i];
 			k = k + 1;
 		}
-		if( (mas[i] % 10 % 2 != 0) $$( mas[i] % 10 % 2 != 0) && (mas[i] % 10 % 2 != 0))
+		if (num % 2 != 0 && num2 % 2 != 0 && num3 % 2 != 0)
 		{
 			mas3[z] = mas[i];
 			z = z + 1;
 		}
-		
+
 
 
 	}
 	cout << "Второй массив" << endl;
-	for (int i = 0; i < 100; i++)
+	for (i = 0; i < 100; i++)
 	{
-		if (mas3[i] > 100 && mas3[i] != 0 && mas3[i] < 999)
+		if (mas2[i] > 100 && mas2[i] != 0 && mas2[i] < 999)
 		{
 			cout << mas2[i] << "  ";
 		}
@@ -61,15 +63,15 @@ int main()
 	}
 	cout << "Конец второго массива" << endl;
 	cout << "Третий массив" << endl;
-	for (int i = 0; i < 100; i++)
+	for (i = 0; i < 100; i++)
 	{
-		if (mas3[i] > 100 && mas3[i] != 0 && mas3[i] < 999) 
+		if (mas3[i] > 100 && mas3[i] != 0 && mas3[i] < 999)
 		{
 			cout << mas3[i] << "  ";
 		}
 		if (i % 15 == 0 && i > 0)
 		{
-			cout << endl;
+			cout << ' ' << endl;
 		}
 	}
 	cout << "Конец третьго массива" << endl;
