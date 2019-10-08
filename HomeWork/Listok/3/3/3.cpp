be39454a205beffa411a75966bf34a6300158025
Sashahonin <777@6020322.ru>
2019-10-08 11:37:10 +0300

    3

--- HomeWork/Listok/3/3/3.cpp
+++ HomeWork/Listok/3/3/3.cpp
@@ -26,36 +26,90 @@ int main()
 		cout << endl;
 
 	}
-	for (int i = 0; i < 2; i++)
+	int masdop[25];
+	int masdop2[25];
+	int mas2[25];
+	int z = 0;
+	for (int i = 0; i < 5; i++)
 	{
 		for (int j = 0; j < 5; j++)
 		{
 			int p = rand() % 4 + 1;
+			cout << p << endl;
 			switch (p)
 			{
 			case 1:
-				
-					cout<< mas[0][i] << " + " << mas[1][j] << " = " << mas[0][i] + mas[1][j] << endl;
-
-				
+				mas2[z] = mas[0][i] + mas[1][j];
+				masdop[z] = mas[0][i];
+				masdop2[z] = mas[1][j];
+				mas2[z] = mas2[z]*10 + z;
+				z = z + 1;
+				cout<< mas[0][i] << " + " << mas[1][j] << " = " << mas[0][i] + mas[1][j] << endl;
+				break;
 			case 2:
-				
-					cout << mas[0][i] << " - " << mas[1][j] << " = " << mas[0][i] - mas[1][j] << endl;
-
+				mas2[z] = mas[0][i] - mas[1][j];
+				mas2[z] = mas2[z] * 10 + 2;
+				masdop[z] = mas[0][i];
+				masdop2[z] = mas[1][j];
+				z = z + 1;
+				cout << mas[0][i] << " - " << mas[1][j] << " = " << mas[0][i] - mas[1][j] << endl;
+				break;
 				
 			case 3:
-				
+				masdop[z] = mas[0][i];
+				masdop2[z] = mas[1][j];
+				mas2[z] = mas[0][i] / mas[1][j];
+				mas2[z] = mas2[z] * 10 + 3;
 				cout << mas[0][i] << " / " << mas[1][j] << " = " << mas[0][i] / mas[1][j] << endl;
-
-				
+				z = z + 1;
+				break;
 			case 4  : 
-				
-					cout << mas[0][i] << " * " << mas[1][j] << " = " << mas[0][i] * mas[1][j] << endl;
-					
+				masdop[z] = mas[0][i];
+				masdop2[z] = mas[1][j];
+				mas2[z] = mas[0][i] * mas[1][j];
+				mas2[z] = mas2[z] * 10 + 4;
+				z = z + 1;
+				cout << mas[0][i] << " * " << mas[1][j] << " = " << mas[0][i] * mas[1][j] << endl;
+				break;
 				
 
 			}
 		}
 	}
-	
+	int n = 25;
+	for (int i = 1; i < n ; ++i)
+	{
+		for (int r = 0; r < n - i; r++)
+		{
+			if (mas2[r] < mas2[r + 1])
+			{
+				int temp = mas2[r];
+				mas2[r] = mas2[r + 1];
+				mas2[r + 1] = temp;
+			}
+		}
+	}
+	cout << endl;
+	for (int i = 1; i < n; ++i)
+	{
+		int d = abs(mas2[i] % 10);
+		switch (d)
+		{
+		case 1:
+			cout << masdop[d] << " + " << masdop2[d] << " = " << mas2[i]/ 10 << endl;
+			break;
+
+		case 2:
+			cout << masdop[d] << " - " << masdop2[d] << " = " << mas2[i] / 10 << endl;
+			break;
+
+		case 3:
+			cout << masdop[d] << " / " << masdop2[d] << " = " << mas2[i] / 10 << endl;
+			break;
+
+		case 4:
+			cout << masdop[d] << " * " << masdop2[d] << " = " << mas2[i] / 10 << endl;
+			break;
+		}
+	}
 }
